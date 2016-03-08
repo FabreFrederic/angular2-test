d7f3b664fb3145b968779286b58baae457a6ff5e
Frederic Fabre <fabrefrederic@free.fr>
2016-03-08 14:03:50 +0100

    2. The Hero Editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,22 +4,22 @@ id: number;
 name: string;
 }
 @Component({
-    selector: 'my-app',
-    template:`
-        <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
-        </div>
+  selector: 'my-app',
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+    </div>
     `
 })
 
 export class AppComponent {
-    public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+  public title = 'Tour of Heroes';
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
